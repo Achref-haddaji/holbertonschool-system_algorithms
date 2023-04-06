ab8f83ec931b44840e67766a9249a4b133895989
Achref-haddaji <haddaji.achref19@gmail.com>
2023-04-06 16:43:35 +0100

    task2

--- 0x01-graphs/2-graph_add_edge.c
+++ 0x01-graphs/2-graph_add_edge.c
@@ -1,53 +1,86 @@
-#include <stdlib.h>
-#include <stdio.h>
+
+ 
 
 #include "graphs.h"
 
 /**
- * main - Entry point
+ * link_vert - links specified vertices
+ * @save_src: pointer to first vertex
+ * @save_dest: pointer to second vertex
+ * @type: UNIDIRECTIONAL or BIDIRECTIONAL
  *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * Return: 1 or 0
  */
-int main(void)
+int link_vert(vertex_t *save_src, vertex_t *save_dest, edge_type_t type)
 {
-    graph_t *graph;
+	edge_t *new, *cur;
 
-    graph = graph_create();
-    if (!graph)
-    {
-        fprintf(stderr, "Failed to create graph\n");
-        return (EXIT_FAILURE);
-    }
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (0);
+	new->dest = save_dest;
+	new->next = NULL;
+	++save_src->nb_edges;
+	for (cur = save_src->edges; cur && cur->next; cur = cur->next)
+		;
+	if (!cur)
+		save_src->edges = new;
+	else
+		cur->next = new;
+	if (type == BIDIRECTIONAL)
+		return (link_vert(save_dest, save_src, UNIDIRECTIONAL));
+	return (1);
+}
 
-    if (!graph_add_vertex(graph, "San Francisco") ||
-        !graph_add_vertex(graph, "Seattle") ||
-        !graph_add_vertex(graph, "New York") ||
-        !graph_add_vertex(graph, "Miami") ||
-        !graph_add_vertex(graph, "Chicago") ||
-        !graph_add_vertex(graph, "Houston") ||
-        !graph_add_vertex(graph, "Las Vegas") ||
-        !graph_add_vertex(graph, "Boston"))
-    {
-        fprintf(stderr, "Failed to add vertex\n");
-        return (EXIT_FAILURE);
-    }
+/**
+ * exist - find vertices exist
+ * @graph: pointer to graph
+ * @src: first vertex
+ * @dest: second vertex
+ * @save_src: pointer to first vertex
+ * @save_dest: pointer to second vertex
+ *
+ * Return: 1 or 0
+ */
+int exist(graph_t *graph, const char *src, const char *dest,
+		vertex_t **save_src, vertex_t **save_dest)
+{
+	vertex_t *cur;
 
-    if (!graph_add_edge(graph, "San Francisco", "Las Vegas", BIDIRECTIONAL) ||
-        !graph_add_edge(graph, "Boston", "New York", UNIDIRECTIONAL) ||
-        !graph_add_edge(graph, "Miami", "San Francisco", BIDIRECTIONAL) ||
-        !graph_add_edge(graph, "Houston", "Seattle", UNIDIRECTIONAL) ||
-        !graph_add_edge(graph, "Chicago", "New York", BIDIRECTIONAL) ||
-        !graph_add_edge(graph, "Las Vegas", "New York", UNIDIRECTIONAL) ||
-        !graph_add_edge(graph, "Seattle", "Chicago", UNIDIRECTIONAL) ||
-        !graph_add_edge(graph, "New York", "Houston", BIDIRECTIONAL) ||
-        !graph_add_edge(graph, "Seattle", "Miami", BIDIRECTIONAL) ||
-        !graph_add_edge(graph, "San Francisco", "Boston", BIDIRECTIONAL))
-    {
-        fprintf(stderr, "Failed to add edge\n");
-        return (EXIT_FAILURE);
-    }
+	cur = graph->vertices;
+	while (cur && (!*save_src || !*save_dest))
+	{
+		if (!(strcmp(cur->content, src)))
+			*save_src = cur;
+		if (!(strcmp(cur->content, dest)))
+			*save_dest = cur;
+		cur = cur->next;
+	}
+	if (!*save_src || !*save_dest)
+		return (0);
+	return (1);
+}
 
-    graph_display(graph);
+/**
+ * graph_add_edge - add an edge
+ * @graph: pointer to graph
+ * @src: first vertex
+ * @dest: second vertex
+ * @type: UNIDIRECTIONAL or BIDIRECTIONAL
+ *
+ * Return: 1 or 0
+ */
+int graph_add_edge(graph_t *graph, const char *src, const char *dest,
+		edge_type_t type)
+{
+	vertex_t *save_src, *save_dest;
 
-    return (EXIT_SUCCESS);
+	if (!graph || !src || !dest ||
+			(type != UNIDIRECTIONAL && type != BIDIRECTIONAL))
+		return (0);
+	save_src = NULL;
+	save_dest = NULL;
+	if (!exist(graph, src, dest, &save_src, &save_dest))
+		return (0);
+	return (link_vert(save_src, save_dest, type));
 }
